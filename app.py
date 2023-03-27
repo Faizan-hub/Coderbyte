73a6802989c66afb3ccdb1150973807d928f4897
Faizan Riasat <56767185+Faizan-hub@users.noreply.github.com>
2023-03-27 12:42:23 +0500

    update app.py

--- app.py
+++ app.py
@@ -3,19 +3,19 @@ from flask import Flask, request
 
 
 app = Flask(__name__)
-model = tf.keras.models.load_model('saved_model.pb')
+# model = tf.keras.models.load_model('saved_model.pb')
 @app.route('/predict', methods=['POST'])
-def predict():
+def inference():
     # Get the image file from the request
-    file = request.files['image_name']
+#     file = request.files['image_name']
 
-    # Preprocess the image
-    image = Image.open(file)
-    image_array = preprocess_image(image)
+#     # Preprocess the image
+#     image = Image.open(file)
+#     image_array = preprocess_image(image)
 
-    # Make a prediction using the model
-    prediction = model.predict(image_array)[0]
-    digit = np.argmax(prediction)
+#     # Make a prediction using the model
+#     prediction = model.predict(image_array)[0]
+#     digit = np.argmax(prediction)
 
     # Return the prediction as a JSON object
-    return {'prediction': str(digit)}
+    return {'prediction': "yess"}
