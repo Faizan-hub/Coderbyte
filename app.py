5569db95978ac979ab35a4a12d4448b10157482e
Faizan Riasat <56767185+Faizan-hub@users.noreply.github.com>
2023-03-27 13:51:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
-import torch
-import tensorflow as tf
+
 from PIL import Image
+import cv2
 import numpy as np
 
 
@@ -12,7 +12,23 @@ def init():
     global model
     
     model = tf.saved_model.load("saved_model.pb")
-    
+def preprocess_image(image):
+    # Convert to RGB format if needed
+    if image.shape[-1] == 3 and image.dtype == np.uint8:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Resize to 224x224 with bilinear interpolation
+    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_LINEAR)
+
+    # Divide by 255
+    image = image / 255.0
+
+    # Normalize using mean and standard deviation values per channel
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    image = (image - mean) / std
+
+    return image
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
@@ -33,4 +49,5 @@ def inference(model_inputs:dict) -> dict:
 
 
     # Return the results as a dictionary
+  
     return {'image_base64': str(digit)}
