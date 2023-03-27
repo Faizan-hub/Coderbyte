6e18e2164f2bfffe376a4bfaacdb1760e381f320
Faizan Riasat <56767185+Faizan-hub@users.noreply.github.com>
2023-03-27 12:50:46 +0500

    update app.py

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
-import json
-from flask import Flask, request
+import os
+import torch
+import base64
+from io import BytesIO
+from transformers import pipeline
+from diffusers import StableDiffusionPipeline
 
 
-app = Flask(__name__)
-# model = tf.keras.models.load_model('saved_model.pb')
-@app.route('/predict', methods=['POST'])
-def inference():
-    # Get the image file from the request
-#     file = request.files['image_name']
+# # Init is ran on server startup
+# # Load your model to GPU as a global variable here using the variable name "model"
+# def init():
+#     global model
+    
+#     model_name = os.getenv("MODEL_NAME")
+#     model = StableDiffusionPipeline.from_pretrained(model_name).to("cuda")
+    
 
-#     # Preprocess the image
-#     image = Image.open(file)
-#     image_array = preprocess_image(image)
+# Inference is ran for every server call
+# Reference your preloaded global model variable here.
+def inference(model_inputs:dict) -> dict:
+    global model
 
-#     # Make a prediction using the model
-#     prediction = model.predict(image_array)[0]
-#     digit = np.argmax(prediction)
+    # Parse out your arguments
+    prompt = model_inputs.get('image_name', None)
+    if prompt == None:
+        return {'message': "No prompt provided"}
+    
 
-    # Return the prediction as a JSON object
-    return {'prediction': "yess"}
+    # Return the results as a dictionary
+    return {'image_base64': prompt}
