04241ecbab4353cc349fd6c3c586212854119c65
Faizan Riasat <56767185+Faizan-hub@users.noreply.github.com>
2023-03-27 13:35:37 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 import torch
 import tensorflow as tf
+from PIL import Image
+import numpy as np
 
 
 
@@ -9,19 +11,26 @@ import tensorflow as tf
 def init():
     global model
     
-    loaded_model = tf.saved_model.load("saved_model.pb")
+    model = tf.saved_model.load("saved_model.pb")
     
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
+    
     global model
 
     # Parse out your arguments
     prompt = model_inputs.get('image_name', None)
     if prompt == None:
         return {'message': "No prompt provided"}
-    
+    image = Image.open(prompt)
+    image_array = preprocess_image(image)
+
+    # Make a prediction using the model
+    prediction = model.predict(image_array)[0]
+    digit = np.argmax(prediction)
+
 
     # Return the results as a dictionary
-    return {'image_base64': prompt}
+    return {'image_base64': str(digit)}
